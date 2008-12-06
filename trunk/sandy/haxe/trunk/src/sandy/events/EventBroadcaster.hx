18df8c5a90ba66e9c6646ebeebf1b7e3fcb285b2
cyanescent <cyanescent@41cf0175-4325-0410-a360-f7e2151edfc1>
2008-12-06 13:52:28 +0000

    A rewrite of the EventBroadcaster, that is more type-friendly.

--- trunk/sandy/haxe/trunk/src/sandy/events/EventBroadcaster.hx
+++ trunk/sandy/haxe/trunk/src/sandy/events/EventBroadcaster.hx
@@ -18,221 +18,93 @@ package sandy.events;
 
 import flash.events.Event;
 
-import sandy.commands.Delegate;
-
-/**
- * Perhaps a dev can enlighten us ;)
- *
- * 
- */
-class EventBroadcaster
+class EventListener
 {
-    private var m_oAll:Hash<Dynamic>;
-    private var m_oType:Hash<Hash<Dynamic>>;
-    private var m_oEventListener:Hash<Hash<Dynamic>>;
-    private var m_oDelegateDico:Hash<Dynamic>;
-    private var listeners:Array<Dynamic>;
+   public var mListner : Dynamic->Void;
+   static var sIDs = 1;
+   public var mID:Int;
 
-    public function new()
-    {
-     m_oAll = new Hash();
-     m_oType = new Hash();
-     m_oEventListener = new Hash();
-     m_oDelegateDico = new Hash();
-    }
+   public function new(inListener)
+   {
+      mListner = inListener;
+      mID = sIDs++;
+   }
 
-    public function isRegistered(listener:Dynamic, ?type:String):Bool
-	{
-		if (type == null)
-		{
-			return m_oAll.get(listener);
-		}
-		else
-		{
-			if (m_oType.get(type) != null)
-			{
-				for (lElt in m_oType.get(type))
-				{
-					if (lElt == listener)
-					{
-						return true;
-					}
-				}
+   public function Is(inListener)
+   {
+      return Reflect.compareMethods(mListner,inListener);
+   }
 
-				return false;
-			}
-			else
-			{
-				return false;
-			}
-		}
-	}
-
-    public function removeListenerCollection(type:String):Void
-	{
-		m_oType.set(type, null);
-	}
-
-    public function getListenerCollection(?type:String):Hash<Dynamic>
-	{
-		return (type != null) ? m_oType.get(type) : m_oAll;
-	}
-
-	public function addEventListener(type:String, listener:Dynamic, ?rest:Array<Event>):Bool
-	{
-
-		if (Reflect.isFunction(listener))
-		{
-			var d:Delegate = new Delegate(listener);
-
-			if (rest != null)
-			{
-				d.setArgumentsArray(rest);
-			}
-
-			m_oDelegateDico.set(Std.string(listener), d);
-			listener = d;
-
-		}
-		else if (listener.hasOwnProperty(type) && (Reflect.isFunction(listener.get(type))))
-		{
-			//
-
-		}
-		else if (listener.hasOwnProperty("handleEvent") && Reflect.isFunction(listener.handleEvent))
-		{
-			//
-
-		}
-		else
-		{
-			return false;	//ERROR CASE
-		}
-
-		if (!isRegistered(listener))
-		{
-			if ((m_oType.get(type) == null))
-			{
-				m_oType.set(type, new Hash());
-			}
-			// --
-			var lDico:Hash<Dynamic> = getListenerCollection(type);
-
-			if (lDico.get(listener) == null)
-			{
-				lDico.set(listener, listener);
-				_storeRef(type, listener);
-				return true;
-			}
-		}
-
-		return false;
-	}
-
-	public function hasListenerCollection(type:String):Bool
-	{
-		return (m_oType.get(type) != null);
-	}
-
-	public function removeEventListener(type:String, listener:Dynamic):Bool
-	{
-		if (hasListenerCollection(type))
-		{
-			var c:Hash<Dynamic> = getListenerCollection(type);
-			if (Reflect.isFunction(listener))
-			{
-				listener = m_oDelegateDico.get(Std.string(listener));
-			}
-			// --
-			if (c.get(listener) != null)
-			{
-				_removeRef(type, listener);
-				if (isDicoEmpty(c))
-				{
-					removeListenerCollection(type);
-				}
-
-				c.remove(Std.string(listener));
-
-				return true;
-			}
-			else
-			{
-				return false;
-			}
-		}
-		else
-		{
-			return false;
-		}
-	}
-
-	public function broadcastEvent(e:Event):Void
-	{
-		if (hasListenerCollection(e.type))
-		{
-			_broadcastEvent( getListenerCollection(e.type), e );
-		}
-		//
-		if (!isDicoEmpty(m_oAll))
-		{
-			_broadcastEvent(m_oAll, e);
-		}
-	}
-
-	public function _broadcastEvent(c:Hash<Dynamic>, e:Event):Void
-	{
-		var type:String = e.type;
-
-		for (listener in c)
-		{
-			var f : Dynamic = null; 	
-			if (Reflect.hasField( listener, "get" ) ) f = listener.get(type);
-
-			if (listener.hasOwnProperty(type) && Reflect.isFunction(f))
-			{
-				f(e);
-			}
-			else if (listener.hasOwnProperty("handleEvent") && Reflect.isFunction(listener.handleEvent))
-			{
-				listener.handleEvent(e);
-
-			}
-			else
-			{
-				//ERROR
-			}
-		}
-	}
-
-	private function _removeRef(type:String, listener:Dynamic):Void
-	{
-		var m:Hash<Dynamic> = m_oEventListener.get(Std.string(listener));
-		m.remove( type );
-
-		if (isDicoEmpty(m))
-		{
-			m_oEventListener.remove( Std.string(listener) );
-		}
-	}
-
-	private function _storeRef(type:String, listener:Dynamic):Void
-	{
-		if (m_oEventListener.get(listener) == null)
-		{
-			m_oEventListener.set(listener, new Hash());
-		}
-		m_oEventListener.get(Std.string(listener)).set(type, listener);
-	}
-
-	private function isDicoEmpty(pDico:Hash<Dynamic>):Bool
-	{
-		var i:Int = 0;
-		for (lElt in pDico)
-		{
-			i++;
-		}
-		return (i == 0);
-	}
+   public function dispatchEvent(event : Event)
+   {
+      mListner(event);
+   }
 }
+
+typedef EventListenerList = Array<EventListener>;
+
+typedef EventMap = Hash<EventListenerList>;
+
+class EventBroadcaster 
+{
+   var mEventMap : EventMap;
+
+   public function new() : Void
+   {
+      mEventMap = new EventMap();
+   }
+
+			public function addEventListener(type:String, listener:Dynamic->Void):Bool
+   {
+      var list = mEventMap.get(type);
+      if (list==null)
+      {
+         list = new EventListenerList();
+         mEventMap.set(type,list);
+      }
+
+      var l =  new EventListener(listener);
+
+						for ( i in 0...list.length )
+								if (list[i].mID == l.mID ) return false;
+      list.push(l);
+
+      return true;
+   }
+
+			public function broadcastEvent(event:Event):Void
+   {
+      var list = mEventMap.get(event.type);
+      if (list!=null)
+      {
+         for(i in 0...list.length)
+         {
+            var listener = list[i];
+               listener.dispatchEvent(event);
+         }
+      }
+   }
+
+   public function hasEventListener(type : String)
+   {
+      return mEventMap.exists(type);
+   }
+
+			public function removeEventListener(type:String, listener:Dynamic->Void):Bool
+   {
+      if (!mEventMap.exists(type)) return false;
+
+      var list = mEventMap.get(type);
+      for(i in 0...list.length)
+      {
+         if (list[i].Is(listener))
+         {
+             list.splice(i,1);
+             return true;
+         }
+      }
+						return false;
+   }
+}
+
 

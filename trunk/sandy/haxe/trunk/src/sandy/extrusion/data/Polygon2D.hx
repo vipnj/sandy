2e3007d600383ccc40537d36d44134798836fa4f
cyanescent <cyanescent@41cf0175-4325-0410-a360-f7e2151edfc1>
2009-01-04 14:35:37 +0000

    Fix to pass javascript testcase (convexHull).

--- trunk/sandy/haxe/trunk/src/sandy/extrusion/data/Polygon2D.hx
+++ trunk/sandy/haxe/trunk/src/sandy/extrusion/data/Polygon2D.hx
@@ -260,7 +260,11 @@ class Polygon2D
 				return new Polygon2D (pointsHolder);
 
 			// lexicographic sort
+#if flash
 			untyped pointsHolder.sortOn (["x", "y"], Array.NUMERIC);
+#else
+			pointsHolder.sort( function(a,b) {return ( a.x>b.x || ( a.x==b.x && a.y>b.y ) ) ? 1: ( a.x<b.x || ( a.x==b.x && a.y<b.y ) ) ? -1 : 0;} );
+#end
 
 			// compute top part of hull
 			topHull.push (0);

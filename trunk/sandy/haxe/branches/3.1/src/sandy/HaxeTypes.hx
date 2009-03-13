c1191cb2b0694c9355b75744dd3eb2e0a7c7f4d9
cyanescent <cyanescent@41cf0175-4325-0410-a360-f7e2151edfc1>
2009-03-13 07:53:46 +0000

    Creating a stable branch. 3.1 AS3 code will be ported to this branch, the current haxe trunk will be used for the 3.2 milestone.

---
+++ trunk/sandy/haxe/branches/3.1/src/sandy/HaxeTypes.hx
@@ -0,0 +1,38 @@
+package sandy;
+
+#if flash10
+typedef TypedArray<T> = flash.Vector<T>;
+#else
+typedef TypedArray<T> = Array<T>;
+#end
+
+#if SANDY_USE_FAST_MATH
+typedef TRIG = sandy.math.FastMath;
+#else
+typedef TRIG = Math;
+#end
+
+class Haxe {
+	#if flash10
+	private static inline var MEM_POOL_LENGTH : Int = 10240;
+	private static var MEM_POOL:flash.utils.ByteArray;
+	#end
+
+	#if flash10
+	public static inline function toTypedArray<T>(v : Array<T>) : TypedArray<T> {
+		return untyped __vector__(v);
+	}
+	#else
+	public static inline function toTypedArray<T>(v : Array<T>) : TypedArray<T> {
+		return v;
+	}
+	#end
+
+	static function __init__() {
+		#if flash10
+			MEM_POOL = new flash.utils.ByteArray();
+			MEM_POOL.length = MEM_POOL_LENGTH;
+			flash.Memory.select(MEM_POOL);
+		#end
+	}
+}
